3cd4a421f9fb4a48d4016e8c53a0ec03011e3387
Joseph <JPMeehan5@Gmail.com>
2024-07-26 21:34:29 -0700

    35-xp-schema

--- src/datamodels/module/data/actor-npc.mjs
+++ src/datamodels/module/data/actor-npc.mjs
@@ -16,11 +16,6 @@ export default class BoilerplateNPC extends BoilerplateActorBase {
       initial: 1,
       min: 0,
     });
-    schema.xp = new fields.NumberField({
-      ...requiredInteger,
-      initial: 0,
-      min: 0,
-    });
 
     return schema;
   }
